06b1a8db18c044546e25f30ba4dcf97763f1b845
sourtaste000 <me@sourtaste000.dev>
2022-02-14 10:45:38 -0800

    remove unused dependencies

--- src/auth/ribbon.mjs
+++
@@ -1,6 +0,0 @@
-import ws from "ws";
-import fetch from "node-fetch";
-
-//fetch https://tetr.io/api/server/environment and store the data in a variable
-const enviroment = await fetch("https://tetr.io/api/server/environment");
-console.log(enviroment);
