91f1d3d3e28344de637ec2ecf8713f9bc77cddd2
sourtaste000 <me@sourtaste000.dev>
2022-02-13 22:06:37 -0800

    format quickplay.js

--- src/auth/quickplay.js
+++ src/auth/quickplay.js
@@ -7,11 +7,13 @@ fetch("https://tetr.io/api/rooms/menu", {
   headers: {
     Authorization: `Bearer ${process.env.TOKEN}`,
   },
-}).then((res) => res.json()).then((data) => {
-  if (data.quickplay.total >= 100) {
-    const embed = new MessageBuilder()
-      .setTitle("Super Lobby has engaged in quickplay!")
-      .setDescription(`${data.quickplay.total} players are in the lobby.`);
-    webhook.send(embed);
-  }
-});
+})
+  .then((res) => res.json())
+  .then((data) => {
+    if (data.quickplay.total >= 100) {
+      const embed = new MessageBuilder()
+        .setTitle("Super Lobby has engaged in quickplay!")
+        .setDescription(`${data.quickplay.total} players are in the lobby.`);
+      webhook.send(embed);
+    }
+  });
