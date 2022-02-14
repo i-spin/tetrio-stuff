4fd766154dabf98f883e46541afae92cf04c4478
sourtaste000 <me@sourtaste000.dev>
2022-02-13 22:05:15 -0800

    use node fetch for quickplay.js

---
+++ src/auth/quickplay.js
@@ -0,0 +1,17 @@
+const fetch = require("node-fetch");
+const { Webhook, MessageBuilder } = require("discord-webhook-node");
+
+const webhook = new Webhook(process.env.WEBHOOK_URL);
+
+fetch("https://tetr.io/api/rooms/menu", {
+  headers: {
+    Authorization: `Bearer ${process.env.TOKEN}`,
+  },
+}).then((res) => res.json()).then((data) => {
+  if (data.quickplay.total >= 100) {
+    const embed = new MessageBuilder()
+      .setTitle("Super Lobby has engaged in quickplay!")
+      .setDescription(`${data.quickplay.total} players are in the lobby.`);
+    webhook.send(embed);
+  }
+});
